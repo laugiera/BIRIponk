8f4431b29a324261779f58c0fd37c7f872ca0420
laugiera <laugier.anne.a@gmail.com>
2017-03-31 16:11:17 +0200

    initialisation

---
+++ src/main.c
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+
+void setVideoMode() {
+  if(NULL == SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, BIT_PER_PIXEL, SDL_OPENGL | SDL_RESIZABLE)) {
+    fprintf(stderr, "Impossible d'ouvrir la fenetre. Fin du programme.\n");
+    exit(EXIT_FAILURE);
+  }
+  reshape();
+  glClear(GL_COLOR_BUFFER_BIT);
+  SDL_GL_SwapBuffers();
+}
+
+int main(int argc, char** argv) {
+  if(-1 == SDL_Init(SDL_INIT_VIDEO)) {
+    fprintf(stderr, "Impossible d'initialiser la SDL. Fin du programme.\n");
+    return EXIT_FAILURE;
+  }
+  setVideoMode();
+  SDL_WM_SetCaption("BIRIPONG", NULL);
+
+  int loop = 1;
+
+
+  while(loop) {
+    Uint32 startTime = SDL_GetTicks();
+    glClear(GL_COLOR_BUFFER_BIT);
+    /*dessin code*/
+    SDL_GL_SwapBuffers();
+    /* ****** */
+
+    SDL_Event e;
+    while(SDL_PollEvent(&e)) {
+      switch(e.type) {
+        case SDL_QUIT:
+          loop=0;
+          break;
+        default:
+          break;
+      }
+    }
+
+    Uint32 elapsedTime = SDL_GetTicks() - startTime;
+    if(elapsedTime < FRAMERATE_MILLISECONDS) {
+      SDL_Delay(FRAMERATE_MILLISECONDS - elapsedTime);
+    }
+  }
+
+  SDL_Quit();
+
+  return EXIT_SUCCESS;
+}
